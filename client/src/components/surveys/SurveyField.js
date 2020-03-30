bec61de5a127c844c39211217d57d24c0d123094
Davit Vardanyan <jedavard@gmail.com>
2020-03-30 14:48:17 +0400

    test values removed

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default ({ input, label, meta: {error, touched} }) => (
     <div>
-        <input {...input} autoComplete={'off'} type={'text'} value={'jedavard@gmail.com'}/>
+        <input {...input} autoComplete={'off'} type={'text'}/>
         <label>{label}&nbsp;<i style={{color: 'darkred'}}>{touched && error}</i></label>
     </div>
 )
